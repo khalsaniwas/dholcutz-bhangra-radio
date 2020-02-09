596c71b5ef778b70493ee9e07f63ff54de9625df
khalsaniwas <honeysaini21@gmail.com>
2020-02-08 21:31:43 -0500

    Added navigation bar
    Adjusted app.css to move navigation bar to top of the page.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,30 @@
 import React from 'react';
 import logo from '../images/logo.svg';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl'
+
 
 function Header() {
   return (
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <header className="App-header">
+
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">DholCutz Bhangra Radio</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Browse</Nav.Link>
+          <Nav.Link href="#pricing">Chat Forume</Nav.Link>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-info">Search</Button>
+        </Form>
+      </Navbar>
+
+    </header>
   );
 }
 
