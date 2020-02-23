a60543f236dda6322b9bc7574da9e7feafde3591
khalsaniwas <honeysaini21@gmail.com>
2020-02-22 22:53:31 -0500

    Rebased

---
+++ src/components/content/NowPlaying.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react'
+import { withRouter } from 'react-router-dom'
+import axios from 'axios'
+import apiConfig from '../../apiConfig'
+import Player from './Player.js'
+import './NowPlaying.css'
+
+const NowPlaying = props => {
+  const [nowPlaying, setNowPlaying] = useState(null)
+  
+
+  useEffect(() => {
+    axios({ url: `${apiConfig.apiUrl}/nowplaying/${apiConfig.stationId}`,
+      method: 'GET',
+    })
+      .then(res => {
+        setNowPlaying(res.data)
+        console.log(res.data)
+      })
+      .catch(console.error)
+  }, [])
+
+  if (!nowPlaying) {
+    return <p>Loading stuff...</p>
+  }
+
+  return (
+    <div className="jumbotron text-center">
+      <h4>Now Playing</h4>
+      <Player />
+      <h6>Listeners: { nowPlaying.listeners.current }</h6>
+      <h6>Next Song: { nowPlaying.playing_next.song.text }</h6>
+      
+    </div>
+  )
+}
+
+export default withRouter(NowPlaying);
---
+++ src/components/content/Request.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button'
+import axios from 'axios'
+import apiConfig from '../../apiConfig'
+
+
+const Request = props => {
+  const [requestMessage, setRequestMessage] = React.useState("");
+
+  const requestSong = () => {
+    axios({ url: `${apiConfig.apiUrl}/station/${apiConfig.stationId}/request/${props.requestId}`,
+      method: 'POST',
+    })
+      .then(res => {
+        setRequestMessage(res.data.message)
+      }, res => {
+        setRequestMessage("Request failed!")
+      })
+      .catch(console.error)
+  }
+
+
+
+  return (
+    requestMessage == "" ?
+      <Button
+        className="request-button"
+        variant="success"
+        onClick={requestSong}
+      >request</Button> :
+      <span>{ requestMessage }</span>
+
+  )
+}
+
+export default Request
